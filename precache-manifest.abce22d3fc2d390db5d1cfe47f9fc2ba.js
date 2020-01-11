4863faa78dde39a55ff7e1f349c72457e0a4af5e
Rainforest <rainforestnick@gmail.com>
2020-01-11 10:45:15 +0800

    deploy

---
+++ precache-manifest.abce22d3fc2d390db5d1cfe47f9fc2ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8e665094a22752255f",
+    "url": "/labeling_tools/css/app.339ecd93.css"
+  },
+  {
+    "revision": "51b718bad28725592223",
+    "url": "/labeling_tools/css/chunk-vendors.91ffd18c.css"
+  },
+  {
+    "revision": "615932da8339b54bda66d9acbf08d7f3",
+    "url": "/labeling_tools/img/default.615932da.jpg"
+  },
+  {
+    "revision": "69b850e038efd718a969a79084aa3beb",
+    "url": "/labeling_tools/index.html"
+  },
+  {
+    "revision": "4330a720cdeec1405358",
+    "url": "/labeling_tools/js/about.70048207.js"
+  },
+  {
+    "revision": "3c8e665094a22752255f",
+    "url": "/labeling_tools/js/app.ec93f346.js"
+  },
+  {
+    "revision": "51b718bad28725592223",
+    "url": "/labeling_tools/js/chunk-vendors.8b29650f.js"
+  },
+  {
+    "revision": "6312f0b1d867a94917ed24f3826fe410",
+    "url": "/labeling_tools/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/labeling_tools/robots.txt"
+  }
+]);
