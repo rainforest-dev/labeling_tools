70e97f11b482cded26ddcde6ae6412d9f65224e8
Rainforest <rainforestnick@gmail.com>
2020-01-11 10:44:38 +0800

    add correspond function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,8 +15,16 @@
         v-toolbar-title {{ getToolbarTitle() }}
         v-spacer
         v-toolbar-items
-          v-btn(icon @click="isSettingsMode = !isSettingsMode")
+          v-btn(icon @click="isSettingsMode = !isSettingsMode" depressed)
             v-icon {{ !isSettingsMode ? "mdi-settings" : "mdi-home" }}
+          v-btn(@click="$refs.upload.click()" depressed icon)
+            v-icon mdi-compare
+          input#upload.d-none(
+            type="file"
+            ref="upload"
+            @change="compare"
+            accept=".csv"
+          )
           v-btn(icon @click="exportLabels")
             v-icon mdi-export-variant
       v-container(v-if="!isSettingsMode")
@@ -35,6 +43,12 @@
             )
               v-icon.mr-1 {{ item.icon }}
               div {{ item.labelName }}
+        v-dialog(v-model="isModalVisible" max-width="50%")
+          v-card
+            v-card-title Latest Compare Result
+            v-card-text Corresponded: {{ corresponded }}
+            v-card-text Marked: {{ marked }}
+            v-card-text Total: {{ correspondedResult.length }}
       v-container(v-else)
         v-file-input(label="Dataset" webkitdirectory multiple @change="choosePath")
         ComboBox(
@@ -57,6 +71,8 @@ export default {
     return {
       isSettingsMode: true,
       isLabeling: false,
+      isComparing: false,
+      isModalVisible: false,
       base64: null,
       files: [],
       currentIndex: 0,
@@ -102,9 +118,21 @@ export default {
           labelName: 'Spider',
           icon: 'mdi-spider',
         },
-
       ],
+      correspondedResult: [],
     };
+  },
+  computed: {
+    corresponded() {
+      return this.correspondedResult.reduce(
+        (accumulator, currentValue) => accumulator + (currentValue.correspond !== '' ? currentValue.correspond : 0), 0,
+      );
+    },
+    marked() {
+      return this.correspondedResult.reduce(
+        (accumulator, currentValue) => accumulator + (currentValue.correspond !== '' ? 1 : 0), 0,
+      );
+    },
   },
   methods: {
     getToolbarTitle() {
@@ -129,13 +157,12 @@ export default {
     exportLabels() {
       let csvContent = '\uFEFF';
       this.files.forEach((file) => {
-        csvContent += `${file.file.name}, ${file.label}\r\n`;
+        csvContent += `${file.file.name},${file.label}\r\n`;
       });
       const blob = new Blob(
         [csvContent],
         { type: 'text/csv;charset=utf-8' },
       );
-      console.log(csvContent);
       saveAs(blob, 'labels.csv');
     },
     choosePath(files) {
@@ -162,6 +189,38 @@ export default {
     },
     removeClass(index) {
       if (index >= 0) this.classes.splice(index, 1);
+    },
+    compare(event) {
+      this.isComparing = true;
+      const fileObj = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsText(fileObj);
+      reader.onload = (e) => {
+        const csvString = e.target.result;
+        const csv = csvString.split('\r\n').map(row => row.split(','));
+        console.log(csv);
+        this.correspondedResult = this.files.map((file) => {
+          const index = csv.findIndex(row => row[0] === file.file.name);
+          if (index >= 0 && csv[index][1] !== '') {
+            if (file.label === csv[index][1]) {
+              return {
+                name: file.file.name,
+                correspond: 1,
+              };
+            }
+            return {
+              name: file.file.name,
+              correspond: 0,
+            };
+          }
+          return {
+            name: file.file.name,
+            correspond: '',
+          };
+        });
+        this.isModalVisible = true;
+        console.log(this.correspondedResult);
+      };
     },
   },
   components: {
